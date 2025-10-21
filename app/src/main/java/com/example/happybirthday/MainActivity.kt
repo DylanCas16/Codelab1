8373fd1bfc0a716214834afabc2d762b4edf1cda
DylanCas16 <dylancastrillon@outlook.com>
2025-10-21 20:12:34 +0100

    Added image and padding

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,33 +29,45 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    BirthdayText(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
+            BirthdayImage()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun BirthdayText(name: String, modifier: Modifier = Modifier) {
     Column {
         Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(0.dp, 40.dp),
+            text = "Happy birthday $name!",
+            modifier = modifier.padding(0.dp, 100.dp),
             textAlign = TextAlign.Center,
             fontSize = 70.sp,
             lineHeight = 80.sp
 
         )
         Text(
-            text = "from James",
-            modifier = modifier.align(alignment = Alignment.End),
+            text = "from your best friend",
+            modifier = modifier.align(alignment = Alignment.End).padding(20.dp, 60.dp),
             fontSize = 30.sp
             )
     }
+}
+
+@Composable
+fun BirthdayImage() {
+    Image(
+        painter = painterResource(R.drawable.fondo_de_pantalla),
+        contentDescription = "Wallpaper",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize(),
+        alpha = 0.9f
+    )
 }
 
 @Preview(
@@ -59,10 +75,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     showSystemUi = true,
     name = "BirthdayCard"
 )
-
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        Greeting("JAMES")
+        Box(modifier = Modifier.fillMaxSize()){
+            BirthdayImage()
+            Column {
+                BirthdayText("James")
+            }
+
+
+        }
     }
 }
