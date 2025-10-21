745164a8c97b162a7c037f880b0b9a08a6c3ef93
DylanCas16 <dylancastrillon@outlook.com>
2025-10-21 19:13:26 +0100

    Birthday card project

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(0.dp, 40.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 70.sp,
+            lineHeight = 80.sp
+
+        )
+        Text(
+            text = "from James",
+            modifier = modifier.align(alignment = Alignment.End),
+            fontSize = 30.sp
+            )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "BirthdayCard"
+)
+
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        Greeting("JAMES")
+    }
+}
